898262e73f06d52ade9b56b26214a0c4536929e2
Bruno-Basques <bruno_basques@outlook.com>
2024-09-27 00:04:01 -0300

    Add entities, and import seeding.

---
+++ src/main/java/com/brunobasques_jsp/second_challange/SecondChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.brunobasques_jsp.second_challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondChallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunobasques_jsp/second_challange/SecondChallangeApplication/SecondChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobasques_jsp.second_challange.SecondChallangeApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
